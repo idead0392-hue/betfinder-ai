1b90f587772212bd9bb4b6635a48d6ab11f31812
idead0392-hue <idead0392@gmail.com>
2025-10-02 11:18:59 -0500

    Add get_esport_tournament function and testUpdate ai_integration_test.py

--- ai_integration_test.py
+++ ai_integration_test.py
@@ -57,6 +57,28 @@ def get_esport_team(team_id, api_key, api_host):
     data = res.read()
     return data.decode("utf-8")
 
+def get_esport_tournament(tournament_id, api_key, api_host):
+    """
+    Get esport tournament information using RapidAPI.
+    
+    Args:
+        tournament_id: The esport tournament ID
+        api_key: RapidAPI key
+        api_host: RapidAPI host
+    
+    Returns:
+        str: JSON response data
+    """
+    conn = http.client.HTTPSConnection(api_host)
+    headers = {
+        'x-rapidapi-key': api_key,
+        'x-rapidapi-host': api_host
+    }
+    conn.request("GET", f"/api/esport/tournament/{tournament_id}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
+
 def get_esport_season_info(tournament_id, season_id, api_key, api_host):
     """
     Get esport season information using RapidAPI.
@@ -109,6 +131,12 @@ if __name__ == "__main__":
     print("Esport Team API Test")
     print("=" * 60 + "\n")
     result = get_esport_team('363904', RAPIDAPI_KEY, RAPIDAPI_HOST)
+    print(result)
+    
+    print("\n" + "=" * 60)
+    print("Esport Tournament API Test")
+    print("=" * 60 + "\n")
+    result = get_esport_tournament('16026', RAPIDAPI_KEY, RAPIDAPI_HOST)
     print(result)
     
     print("\n" + "=" * 60)
